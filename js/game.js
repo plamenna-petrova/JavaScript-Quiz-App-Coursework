b6da53ab18b7d045a509c14d349de180a75db21b
plamenna.petrova <plamennavp@abv.bg>
2024-04-04 08:48:59 +0300

    Fixed relative paths for deployment

--- js/game.js
+++ js/game.js
@@ -70,7 +70,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score);
         // go to the endPage     
-        return window.location.assign('/html/end.html');
+        return window.location.assign('../html/end.html');
     }
 
     questionCounter++;
@@ -149,7 +149,7 @@ function startTimer(duration, display) {
         if (--timer < 0){
             clearInterval();
             localStorage.setItem("mostRecentScore", score);
-            return window.location.assign('/html/end.html');
+            return window.location.assign('../html/end.html');
         }
     }, 1000);
 }
